c1eaa0adbbc61f193475d6e833a8649b3e99d424
Daniel-Alencar <danielalencar746@gmail.com>
2021-05-26 11:59:26 -0300

    up

--- tests.c
+++ tests.c
@@ -5,8 +5,14 @@
 int main(){
   char teste[] = "lw $s0, 5($s1)";
   char *first;
+  int lenght;
 
   first = strchr(teste, '(');
 
   printf("%s\n", first);
+  printf("# %s\n", teste + strlen(first));
+
+  for(lenght = 0; strcmp(teste + strlen(teste) - strlen(first) - lenght, ","); lenght++){
+    printf("$ %d\n", lenght);
+  }
 }
