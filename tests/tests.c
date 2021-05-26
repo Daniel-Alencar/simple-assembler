a44be43b0200d4630b1fcb007377d4d44dcbb208
GkingOficial <talisoncaedoso@gmail.com>
2021-05-26 17:36:58 -0300

    finalmente consegui!

--- tests/tests.c
+++ tests/tests.c
@@ -3,20 +3,18 @@
 #include <string.h>
 
 int main(){
-  char teste[] = "lw $s0, 555($s1)";
+  char teste[50] = "lw $s0, 555($s1)";
   char *first, *aux;
   int lenght;
 
   first = strchr(teste, '(');
 
-  for(lenght = 0; !(aux = strpbrk(teste + strlen(teste) - strlen(first) - lenght, ", ")); lenght++);
+  for(int i = 0; !(aux = strpbrk(teste + strlen(teste) - i, ", ")); i++);
   
-  //memset(teste + strlen(teste) - strlen(aux), ' ', lenght);
-  printf("%s\n", aux);
+  lenght = strlen(aux) - strlen(first);
+  
   strncat(teste, aux + 1, lenght - 1);
-
-  //memset(aux, '-', lenght);
+  memset(aux, ' ', lenght);
 
   printf("%s\n", teste);
-  printf("%d\n", lenght);
 }
