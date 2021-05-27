f37255dc185741af92e028061b830473915f038e
GkingOficial <talisoncaedoso@gmail.com>
2021-05-27 10:19:41 -0300

    funções lógicas implementadas juntas em logicalFunctions

--- tests/logicalfunctions.c
+++ tests/logicalfunctions.c
@@ -4,22 +4,29 @@
 void logicalFunctions(char *, char *, char *, char *);
 
 int main (){
-    char string1[] = "10101", string2[] = "11011", string[5];
-    char label[] = "xor";
+    char string1[] = "10101", string2[] = "11011", string[5], label[] = "xor";
 
     logicalFunctions(string1, string2, string, label);
     
-    printf("%s\n", string);
+    printf("%s", string);
 }
 
 void logicalFunctions(char *string1, char *string2, char *string, char *label){
-    for(int i = 0; i < strlen(string1); i++){
-        if(!strcmp(label, "and")){
-            string1[i] == '1' && string2[i] == '1' ? strcat(string + i, "1") : strcat(string + i, "0");
-        } else if(!strcmp(label, "or")){
-            string1[i] == '1' || string2[i] == '1' ? strcat(string + i, "1") : strcat(string + i, "0");
-        } else if(!strcmp(label, "xor")) {
-            string1[i] == '1' ^ string2[i] == '1' ? strcat(string + i, "1") : strcat(string + i, "0");
+    int i = 0;
+
+    if(!strcmp(label, "and")){
+        for(; i < strlen(string1); i++){
+            string1[i] == '1' && string2[i] == '1' ? strcpy(string + i, "1") : strcat(string + i, "0");
         }
+    } else if(!strcmp(label, "or")){
+        for(; i < strlen(string1); i++){
+            string1[i] == '1' || string2[i] == '1' ? strcpy(string + i, "1") : strcat(string + i, "0");
+        }
+    } else if(!strcmp(label, "xor")) {
+        for(; i < strlen(string1); i++){
+            string1[i] == '1' ^ string2[i] == '1' ? strcpy(string + i, "1") : strcat(string + i, "0");
+        }
+    } else {
+        return;
     }
 }
