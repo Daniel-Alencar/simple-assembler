92dff88338efce8e6265f0648f53796edd75b344
GkingOficial <talisoncaedoso@gmail.com>
2021-06-01 10:33:24 -0300

    J pronto

--- Types/J.c
+++ Types/J.c
@@ -3,53 +3,47 @@
 #include <stdlib.h>
 #include "../Libs/useful lib/useful.c"
 
+char **labels;
+int *labelsPositions;
+
+void instructionJ(char *instruction, char *opcode);
+
 int main() {
+  int i;
   char instruction[] = "j for";
-
-  int i, j;
-  int row = 3;
-
-  char **partsOfIntruction[2];
-  char **labels;
-  int *labelsPositions;
-
-  char *opcode = (char *) malloc(sizeof(char) * 6);
-  char *instructionInBinary[33];
-
-  int linhaDoLabelNoArquivo;
-  char *binaryString = (char *) malloc(sizeof(char) * 27);
-
-  // Preparativos
 
   labels = (char **) malloc(sizeof(char *) * 2);
   for(i = 0; i < 2; i++) {
     labels[i] = (char *) malloc(sizeof(char) * 10);
   }
-  strcpy(labels[0], "for");
-
   labelsPositions = (int *) malloc(sizeof(int) * 10);
+
+  strcpy(labels[0], "for");
   labelsPositions[0] = 5;
+  
+  instructionJ(instruction, "000010");
+}
 
-  partsOfIntruction[0] = (char *) malloc(sizeof(char) * 7);
-  partsOfIntruction[1] = (char *) malloc(sizeof(char) * 27);
+void instructionJ(char *instruction, char *opcode){
+  int i;
+  char targetAddress[27];
+  char instructionInBinary[33];
+  char label[10];
 
-  // Identificar o tipo da instrução e o opcode na leitura do arquivo.txt
-
-  strcpy(opcode, "000010");
-  strcpy(partsOfIntruction[0], opcode);
-
+  char *binaryString = (char *) malloc(sizeof(char) * 27);
+  
   // Leitura e procura do 'for' no labels, identificar a posição e procurar valor correspondente em linhaDosLabels
+  sscanf(instruction, "%*s %s", label);
 
-  for(i = 0; strcmp(labels[i], "for") != 0; i++);
-  linhaDoLabelNoArquivo = labelsPositions[i];
+  for(i = 0; strcmp(labels[i], label); i++);
 
   // Manipular o valor da linhaDosLabels para formar o target address
 
-  binaryString = convertDecimalToBinary(linhaDoLabelNoArquivo);
-
-  sprintf(partsOfIntruction[1], "%026s", binaryString);
-  strcpy(instructionInBinary, partsOfIntruction[0]);
-  strcat(instructionInBinary, partsOfIntruction[1]);
+  binaryString = convertDecimalToBinary(labelsPositions[i]);
+  
+  strcpy(instructionInBinary, opcode);
+  sprintf(targetAddress, "%026s", binaryString);
+  strcat(instructionInBinary, targetAddress);
 
   printf("%s\n", instructionInBinary);
 }
