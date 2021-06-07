b37db7a659a3c9a4a2e6229b23759e7bd0200e73
GkingOficial <talisoncaedoso@gmail.com>
2021-06-07 12:40:35 -0300

    label e valores reais funcionando

--- Libs/files lib/cleanfile.c
+++ Libs/files lib/cleanfile.c
@@ -15,50 +15,75 @@ int main(){
 
     cleanLineOfFile(input, str);
 
-    for(int i = 0; i < 3; i++){
-        printf("-%s-\n", labels[i]);
+    for(int i = 0; i < 10; i++){
+        printf("-%s-%d-\n", labels[i], labelsPositions[i]);
     }
 }
 
 void cleanLineOfFile(FILE *file, char *string){
     char *aux;
-    int numberOfLabels = 0, labelPosition = 0;
+    char aux1;
+    int numberOfLabels = 0, linePosition = 0, labelsPositionsCounter = 0;
+    int changeNumberOfLabels = 0;
 
     labels = (char **)malloc(sizeof(char *));
     labelsPositions = (int *)malloc(sizeof(int));
 
     do{
-        fgets(string, 101, file);
+        if(fgets(string, 101, file) == NULL){
+            break;
+        }
         
         //remove comentario da linha
         while(aux = strchr(string, '#')){
             strcpy(aux, "\0");
         }
         
-        //remove tab da linha
-        while(aux = strchr(string, '\t')){
+        //substitui tab por espaço
+        int i = 0;
+        while(aux1 = string[i], aux1 == '\t') {
+            aux = strchr(string, '\t');
+            strcpy(aux, aux + 1);
+            i++;
+        }
+        
+        while(aux = strchr(string, '\t')) {
             aux[0] = ' ';
         }
 
         //se for linha válida
         if(strcmp(string, "\n") && strcmp(string, "\0") && strcmp(string, " ")){
-            labelPosition++;
-    
+            // linePosition++;
             aux = splitLabel(string);
             if(aux){
-
                 numberOfLabels++;
+                changeNumberOfLabels = 1;
 
                 labels = (char **)realloc(labels, sizeof(char *) * numberOfLabels);
                 labels[numberOfLabels - 1] = (char *)malloc(sizeof(char) * strlen(aux));
                 strcpy(labels[numberOfLabels - 1], aux);
 
-                labelsPositions = (int *)realloc(labelsPositions, sizeof(int) * numberOfLabels);
-                labelsPositions[numberOfLabels - 1] = labelPosition;
-
                 //remove espaços do label
                 while(aux = strchr(labels[numberOfLabels - 1], ' ')){
                     strcpy(aux, aux + 1);
+                }
+            }
+
+            //remove labels da string
+            while(aux = strchr(string, ':')){
+                strcpy(string, aux + 1);
+            }
+            //se após remover o label sobrou alguma instrução válida
+            
+            if(strcmp(string, "\n") && strcmp(string, "\0") && strcmp(string, " ")){
+                linePosition++;
+                // printf("||%s||\n", string);
+                
+                if(changeNumberOfLabels) {
+                    labelsPositions = (int *)realloc(labelsPositions, sizeof(int) * numberOfLabels);
+                    labelsPositions[numberOfLabels - 1] = linePosition;
+
+                    changeNumberOfLabels = 0;
                 }
             }
         }
